a1559d8cc7833d79b095fffa7e610a0c38a39bac
Maximilian Lampl <lammab20@htl-kaindorf.at>
2024-11-09 20:00:42 +0100

    first commit

---
+++ src/test/java/at/htlkaindorf/uebung_1/Uebung1ApplicationTests.java
@@ -0,0 +1,13 @@
+package at.htlkaindorf.uebung_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Uebung1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
